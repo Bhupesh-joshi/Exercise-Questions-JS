bd0dd04333b5ddf854f30713c899d9cc939390dd
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-03-11 02:38:03 +0530

    Code with bubb Exercise 1 q1

---
+++ Exercise1/q1.js
@@ -0,0 +1,12 @@
+a = 20;
+b = 30;
+
+if(a+b === 100){
+    console.log("true");
+} else if(a === 100) {
+    console.log("true");
+} else if(b === 100) {
+    console.log("true");
+} else {
+    console.log("false");
+}
